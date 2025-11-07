73e57429bee79aa13082d793b17ef476b7776b3b
Siddhesh <siddheshborse25@gmail.com>
2025-11-07 11:25:52 +0530

    Product schema is created

---
+++ server/models/product.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+    productName :{
+        type : String,
+        required : true
+    },
+    productImage :[{
+        public_id : {
+            type : String,
+            required : true
+        },
+        url : {
+            type : String,
+            required : true
+        }
+    }],
+    productDescription :{
+        type : String,
+        required : true
+    },
+    productDetails: [{
+        color: { 
+            type: String, 
+            required: true 
+        },
+        storage: { 
+            type: Number, 
+            required: true 
+        },
+        ram : {
+            type : Number,
+            required : true
+        },
+        price: { 
+            type: Number, 
+            required: true 
+        },
+        cashback : {
+            type : Number,
+            required : true
+        }
+    }]
+},{
+    timestamps : true
+})
+
+const Product = mongoose.model('Product',productSchema);
+export default Product;
