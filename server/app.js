bbd90627d151f38ed1ec887a4fb86dd0375d6e06
Siddhesh <siddheshborse25@gmail.com>
2025-11-07 10:47:59 +0530

    Basic backend setup is completed

---
+++ server/app.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello world');
+})
+
+export default app;
