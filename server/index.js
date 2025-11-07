666943760ae9a57aaaf3aa96a8f7c38d7ca3ae4f
Siddhesh <siddheshborse25@gmail.com>
2025-11-07 11:00:53 +0530

    DB connected

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ import http from 'http';
 import app from './app.js';
 const port = process.env.PORT || 4000;
 
-const server = http.createServer(app);;
+import connectToDB from './db/conn.js';
+connectToDB(process.env.DB_CONNECT)
+
+const server = http.createServer(app);
 
 server.listen(port,()=>{
     console.log(`Server is running on the port ${port}`);
